47de3d4f541a6a5a95720bd7b3031ca0e73fc883
Kaio G <kaiogreco91@gmail.com>
2022-11-08 21:21:24 -0300

    feat: :art: scrape_next_page_link
    função responsável por buscar o link de nova página

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -17,12 +17,13 @@ def fetch(url: str) -> str:
 # Requisito 2
 def scrape_novidades(html_content):
     selector = Selector(html_content)
-    return selector.css('.entry-preview .cs-overlay-link::attr(href)').getall()
+    return selector.css(".entry-preview .cs-overlay-link::attr(href)").getall()
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    return selector.css(".next.page-numbers::attr(href)").get()
 
 
 # Requisito 4
@@ -33,3 +34,10 @@ def scrape_noticia(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+if __name__ == "__main__":
+    quotes = scrape_next_page_link("https://blog.betrybe.com/")
+
+    # page_content = fetch("https://blog.betrybe.com/")
+    # news = scrape_novidades(page_content)
