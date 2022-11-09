be66fd527ab6bd90eb9d05c60b608dd13abb0ab2
Kaio G <kaiogreco91@gmail.com>
2022-11-09 16:55:55 -0300

    feat: :construction: scapre_noticia
    função responsável por realizar o scrape dos dados necessários
    
    função precisa de alguns ajustes para passar no teste

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -27,17 +27,27 @@ def scrape_next_page_link(html_content):
 
 
 # Requisito 4
-def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+def scrape_noticia(html_content: str) -> list:
+    selector = Selector(html_content)
+
+    news_details = {
+        "url": selector.css("link[rel=canonical]::attr(href)").get(),
+        "title": selector.css("h1.entry-title::text").get(),
+        "timestamp": selector.css("li.meta-date::text").get(),
+        "writer": selector.css("a.url.fn.n::text").get(),
+        "comments_count": 0,
+        "summary": selector.css("div.entry-content").get(),
+        "tags": selector.css(
+            "section.post-tags > ul > li > a::text"
+        ).getall(),
+        "category": selector.css(
+            "a.category-style > span.label::text"
+        ).get(),
+    }
+
+    return news_details
 
 
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
-
-
-if __name__ == "__main__":
-    quotes = scrape_next_page_link("https://blog.betrybe.com/")
-
-    # page_content = fetch("https://blog.betrybe.com/")
-    # news = scrape_novidades(page_content)
